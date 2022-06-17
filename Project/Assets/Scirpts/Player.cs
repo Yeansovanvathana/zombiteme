075beb9a809c1a33c3790dd5cd5f3fcd3c11d684
hengvisoth <hengratanakvisoth20@kit.edu.kh>
2022-06-17 10:49:50 +0700

    GameMenu added

--- Project/Assets/Scirpts/Player.cs
+++ Project/Assets/Scirpts/Player.cs
@@ -7,7 +7,8 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float dirX, dirY;
-    public float moveSpeed = 5f;
+    public float moveSpeed = 4f;
+    public float Jump = 5.5f;
 
     public bool ClimbingAllowed { get; set; }
     // Start is called before the first frame update
@@ -22,7 +23,7 @@ public class Player : MonoBehaviour
         dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
         if (ClimbingAllowed)
         {
-            dirY = Input.GetAxis("Vertical") * moveSpeed;         
+            dirY = CrossPlatformInputManager.GetAxis("Vertical") * Jump;         
         }
     }
     private void FixedUpdate()
